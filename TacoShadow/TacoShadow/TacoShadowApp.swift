b3d698fae826e83460867793ae20e906632b6ca2
Abdeltwab Mohamad <Abdeltwab.elhussin@gmail.com>
2024-06-06 11:27:24 +0300

    add inital project

---
+++ TacoShadow/TacoShadow/TacoShadowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TacoShadowApp.swift
+//  TacoShadow
+//
+//  Created by abdeltwab mohamed abdeltwab elhussin on 06/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TacoShadowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
