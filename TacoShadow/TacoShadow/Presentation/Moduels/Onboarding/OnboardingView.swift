ee63d8d2b6b9d7ad2f28e21762f23c977dff507a
Ahmed <ahmad.elhussin03@gmail.com>
2024-06-15 15:21:23 +0300

    add onboring

--- TacoShadow/TacoShadow/Presentation/Moduels/Onboarding/OnboardingView.swift
+++ TacoShadow/TacoShadow/Presentation/Moduels/Onboarding/OnboardingView.swift
@@ -2,11 +2,11 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
+        
         ZStack {
             Image(AppImage.onboarding1.rawValue)
                 .resizable()
                 .ignoresSafeArea()
-            
             
             VStack {
                 HStack {
@@ -16,6 +16,31 @@ struct OnboardingView: View {
                         .foregroundColor(Color(AppColor.primary25.rawValue))
                 }
                 Spacer()
+                
+                
+                VStack{
+                    
+                    Text("Discover Your Favorite ")
+                        .foregroundColor(Color(AppColor.primary25.rawValue))
+                        .font(.system(size: 27))
+                        .fontWeight(.bold)
+                        .padding(.leading)
+                    
+                    Text("Flavors")
+                        .foregroundColor(Color(AppColor.primary25.rawValue))
+                        .font(.system(size: 27))
+                        .fontWeight(.bold)
+                                
+                    Text("Next")
+                        .frame(width: 340 , height: 60)
+                        .background(.red)
+                        .cornerRadius(30)
+                        .foregroundColor(.white)
+                        .font(.system(size: 20))
+                    
+                    
+                }.padding()
+                
             }
             .padding(.trailing,24)
             .padding(.top,24)
